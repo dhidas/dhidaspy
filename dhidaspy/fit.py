5988b7b1ec2e8d425b4fa4a19eb2c1fcf29a58f6
Dean Andrew Hidas <dhidas@bnl.gov>
2020-08-13 17:11:48 -0400

    hi

---
+++ dhidaspy/fit.py
@@ -0,0 +1,32 @@
+
+def fwhm (x, y, ip=None):
+    """
+    Find the full width half max given x and y arrays.  Optionally specify a point ip of a local max
+
+    x : list/array
+    y : list/array
+    ip : index of point about which to find fwhm (better be a max)
+    """
+
+    if ip is None:
+        ip = y.index(max(y))
+
+    halfmax = y[ip] / 2.
+    print(ip, x[ip], y[ip], max(y), halfmax)
+
+    for i in range(ip, 0, -1):
+        if y[i] < halfmax:
+            slope = (y[i + 1] - y[i]) / (x[i + 1] - x[i])
+            dy = (halfmax - y[i]) / (y[i + 1] - y[i])
+            x1 = x[i + 1] - dy / slope
+            print(x1)
+            break
+    for i in range(ip, len(x), 1):
+        if y[i] < halfmax:
+            slope = (y[i - 1] - y[i]) / (x[i - 1] - x[i])
+            dy = (halfmax - y[i]) / (y[i - 1] - y[i])
+            x2 = x[i - 1] - dy / slope
+            print(dy/slope)
+            break
+
+    return x2 - x1
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name = 'dhidaspy',
+    version = '0.2',
+    packages=['dhidaspy'],
+)
+
